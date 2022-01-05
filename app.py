5da2e800f25d25d5518a1068a14feecc52189425
Bill <billmewtwo1996@gmail.com>
2022-01-05 19:16:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import psycopg2
 import json
 import requests
+from psycopg2.extras import Json
+
 
 
 def etl():
-    connection = psycopg2.connect("postgres://postgres:e26519982@localhost:5432/postgres")
+    connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
     connection.autocommit = True
     crs = connection.cursor()
 
@@ -16,9 +18,9 @@ def etl():
 
 
 def fetch_data():
-    url = 'https://api.fda.gov/animalandveterinary/event.json?limit=50'
+    url = 'https://api.fda.gov/animalandveterinary/event.json?limit=1000'
     response = requests.get(url)
-    #print(url)
+    print(url)
     yield response.json().get("results")
 
     while response.links.get("next") is not None:
@@ -27,7 +29,40 @@ def fetch_data():
         yield response.json().get("results")
 
 
-if __name__ == '__main__':
-    etl()
 
+def etl_dog_api():
+    connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
+    connection.autocommit = True
+    crs = connection.cursor()
+
+    insert_q = """
+     INSERT INTO raw_dog (raw_data)
+       SELECT j FROM jsonb_array_elements((%s)::jsonb) AS j;
+    """
+
+    crs.execute(insert_q, fetch_data_dog_api())
+
+
+def fetch_data_dog_api():
+    url = 'https://api.thedogapi.com/v1/breeds/'
+    response = requests.get(url)
+    response = response.text
+    print(response)
+
+    return [response]
+
+
+if __name__ == '__main__':
+        etl_dog_api()
+
+    # def fetch_data():
+    #     url = 'https://api.fda.gov/animalandveterinary/event.json?limit=1000'
+    #     response = requests.get(url)
+    #     print(url)
+    #     yield response.json().get("results")
+    #
+    #     while response.links.get("next") is not None:
+    #         url = response.links.get('next').get("url")
+    #         response = requests.get(url)
+    #         yield response.json().get("results")
 
