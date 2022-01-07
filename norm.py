60067a2fe1641a310b73e642f1d310db8b5de2a4
Bill <billmewtwo1996@gmail.com>
2022-01-07 23:21:15 +0200

    Update norm.py

--- norm.py
+++ norm.py
@@ -1,164 +1,117 @@
 import psycopg2
+import requests
+import psycopg2
+import json
 
 
 
-# def etl_dog_api():
-#     connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
-#     connection.autocommit = True
-#     crs = connection.cursor()
-#
-#     insert_q = """
-#      INSERT INTO raw_dog (raw_data)
-#        SELECT j FROM jsonb_array_elements((%s)::jsonb) AS j;
-#     """
-#
-#     crs.execute(insert_q, fetch_data_dog_api())
+connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
+connection.autocommit = True
 
-def common_breeds():
-    breeds_list = []
-    connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
-    connection.autocommit = True
+crs = connection.cursor()
 
-    crs = connection.cursor()
+crs.execute("SELECT * FROM raw LIMIT 2000")
 
-    crs.execute("SELECT * FROM raw LIMIT 2000")
-
-    records = crs.fetchall()
-    # print(records)
-
-    for record in records:
-        try:
-            animal = record[1].get("animal")
-            breed = animal['breed']['breed_component']
-            # print(breed)
-
-            breeds_list.append(breed)
-        except Exception as e:
-                print(e)
-    return breeds_list
-        # finally:
-        #     return column_list
+records = crs.fetchall()
 
 
 def common_active_ingredients():
-    active_ingredients_list = []
-    connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
-    connection.autocommit = True
+    # Active Ingredients
+    active_ingredients_to_insert = []
+    for record in records:
+        for drugs_data in record[1].get('drug'):
+            for active_ingredient in drugs_data.get('active_ingredients'):
+                active_ingredients_to_insert.append(active_ingredient.get('name', 'Unknown'))
 
-    crs = connection.cursor()
+    insert_q = """
+        INSERT INTO active_ingredients (active_ingredient)
+        VALUES (%s)
+        ON CONFLICT DO NOTHING
+        """
 
-    crs.execute("SELECT * FROM raw LIMIT 2000")
+    for active_ingredient_to_insert in active_ingredients_to_insert:
+        crs.execute(insert_q, (active_ingredient_to_insert,))
 
-    records = crs.fetchall()
-    # print(records)
 
+
+def active_ingrediends_per_incident():
     for record in records:
         try:
-            active_ingredients = record[1].get("drug")
-            active_ingredients = active_ingredients[0]["active_ingredients"]
-            active_ingredients = active_ingredients[0]["name"]
-            print(active_ingredients)
-            active_ingredients_list.append(active_ingredients)
-        except Exception as e:
-                print(e)
-    return active_ingredients_list
-        # finally:
-        #     return column_list
+            for drugs_data in record[1].get('drug'):
+                for active_ingredient in drugs_data.get('active_ingredients'):
+
+                    crs.execute(f"""
+                    SELECT id FROM active_ingredients
+                    WHERE active_ingredient = '{active_ingredient.get('name', 'Unknown')}'
+                    """)
+
+                    ai_to_insert = crs.fetchone()
+
+                    crs.execute("""
+                    INSERT INTO incident_ai (unique_aer_id_number,ai_id,numenator,numerator_unit,denominator,denominator_unit)
+                    VALUES (%s,%s,%s,%s,%s,%s)
+                    ON CONFLICT DO NOTHING 
+                    """, (record[0], ai_to_insert[0], active_ingredient.get('dose').get('numerator'),
+                        active_ingredient.get('dose').get('numerator'), active_ingredient.get('dose').get('denominator'),
+                        active_ingredient.get('dose').get('denominator_unit')))
+        except Exception as E:
+            print ("Error: {}".format(E))
+
+def animals():
+    for record in records:
+        try:
+
+            # for animal_data in record[1].get('animal'):
+                animal_data = record[1].get("animal")
+                print(animal_data['breed']['breed_component'])
+                print(animal_data['species'])
+                crs.execute(f"""
+                INSERT INTO animals(unique_aer_id_number, species, gender, reproductive_status, 
+                female_animal_physiological_status, type_of_information)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                ON CONFLICT DO NOTHING
+                """, (record[0], animal_data.get('species'), animal_data.get('gender'), animal_data.get('reproductive_status'),
+                     animal_data.get('female_animal_physiological_status'), animal_data.get('type_of_information')))
+
+        except Exception as E:
+            print("Error: {}".format(E))
+
+def age():
+    for record in records:
+        try:
+            animal_data = record[1].get("animal")
+            age = animal_data["age"]
+            print(age)
+            min = age.get("min")
+            print(min)
+            crs.execute(f"""
+            INSERT INTO age_table(unique_aer_id_number, min, unit, qualifier)
+            VALUES (%s, %s, %s, %s)
+            ON CONFLICT DO NOTHING
+            """, (record[0], age.get("min"), age.get("unit"), age.get("qualifier")))
+        except Exception as E:
+            print("Error: {}".format(E))
 
 
-# test =common_active_ingredients()
-# print(test)
-# common_breeds()
+def results():
+    for record in records:
+        try:
+            results = record[1]
+            original_receive_date = results.get("original_receive_date")
+            number_of_animals_affected = results.get("numbers_of_animals_affected")
+            primary_reporter = results.get("primary_reporter")
+            number_of_animals_treated = results.get("number_of_animals_treated")
+            onset_date = results.get("onset_date")
+            report_id = results.get("report_id")
+            # print(report_id)
+            # print(onset_date)
+            print(number_of_animals_affected)
+            print(number_of_animals_treated)
+        except Exception as E:
+            print("Error: {}".format(E))
 
-# def insert_to_column():
-#
-#     connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
-#     connection.autocommit = True
-#     crs = connection.cursor()
-#
-#     insert_q = """
-#      INSERT INTO data_warehouse (breed)
-#        SELECT j FROM unnest((%s)::text[]) AS j;
-#     """
-#     data = set_column()
-#     data = str(data)
-#     for d in data:
-#         crs.execute(insert_q, d)
-#
-#
-# insert_to_column()
-#
 
-# set_column()
-# print(column_list)
-# if __name__ == '__main__':
-#
-#     connection = psycopg2.connect("postgres://postgres:banana_2@localhost:5432/postgres")
-#     connection.autocommit = True
-#
-#     crs = connection.cursor()
-#
-#     crs.execute("SELECT * FROM raw LIMIT 200")
-#
-#     records = crs.fetchall()
-#
-#     # insert_q = """
-#     #  INSERT INTO raw_dog (raw_data)
-#     #    SELECT j FROM jsonb_array_elements((%s)::jsonb) AS j;
-#     # """
-#     #
-#     # crs.execute(insert_q, response)
-#
-#     for record in records:
-#         try:
-#             # my_data = record[0], record[1].get('primary_reporter'),  record[2].get('lot_number')
-#             # print(my_data)
-#             #
-#             animal = record[1].get("animal")
-#             # breed = animal['breed']['breed_component']
-#             breed = animal['breed']['breed_component']
-#             print(breed)
-#             str(breed)
-#             # crs.execute(insert_q, breed)
-#
-#             # original_receive_date = record[1].get("original_receive_date")
-#             # print(original_receive_date)
-#             #
-#             # reaction = record[1].get("reaction")
-#             # reaction = reaction[0].get("veddra_term_name")
-#             # print(reaction)
-#             #
-#             # active_ingredients = record[1].get("drug")
-#             # active_ingredients = active_ingredients[0]["active_ingredients"]
-#             # active_ingredients = active_ingredients[0]["name"]
-#             # print(active_ingredients)
-#             #
-#             #
-#             #
-#             # animal = record[1].get("animal")
-#             # weight = animal['weight']['min']
-#             # print(weight)
-#             #
-#             # animal = record[1].get("animal")
-#             # species = animal['species']
-#             # gender = animal['gender']
-#             # reproductive_status = animal.get('reproductive_status')
-#             # # print(reproductive_status)
-#             # print("Gender: {} | Species: {} | Reproductive Status : {}".format(gender, species, reproductive_status))
-#             #
-#             # geography = record[1].get("receiver")
-#             # city = geography["city"]
-#             # print(city)
-#             #
-#             # time_between_exposure_and_onset = record[1].get("time_between_exposure_and_onset")
-#             # print(time_between_exposure_and_onset)
-#
-#             #brand name
-#             #age
-#             #dose
-#             #used_according_to_label
-#             #duration
-#             #outcome
-#         except Exception as e:
-#             print(e)
-#
+
+if __name__ == "__main__":
+    # common_active_ingredients()
+    age()
