7b6861099c46d1d1fa9cf553df8e738f5678a11c
Bill <billmewtwo1996@gmail.com>
2022-01-16 16:35:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,44 @@ import json
 import requests
 from psycopg2.extras import Json
 
+connection_name = "postgres://postgres:banana_2@localhost:5432/postgres"
+
+
+raw_table = ("""
+CREATE TABLE raw
+(
+ unique_aer_id_number TEXT NOT NULL,
+ raw_data JSON,
+ PRIMARY KEY (unique_aer_id_number)
+);
+
+""")
+
+raw_dog_table = ("""
+CREATE TABLE raw_dog
+(
+  raw_data JSON
+);
+
+""")
+
+table_list = [raw_table, raw_dog_table]
+
+
+def table_creation():
+    try:
+        connection = psycopg2.connect(connection_name)
+        connection.autocommit = True
+        crs = connection.cursor()
+
+        for command in table_list:
+            crs.execute(command)
+            # close communication with the PostgreSQL database server
+            # crs.close()
+            # commit the changes
+            connection.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 
 def etl():
@@ -37,7 +75,7 @@ def etl_dog_api():
 
     insert_q = """
      INSERT INTO raw_dog (raw_data)
-       SELECT j FROM jsonb_array_elements((%s)::jsonb) AS j;
+       SELECT j FROM json_array_elements((%s)::json) AS j;
     """
 
     crs.execute(insert_q, fetch_data_dog_api())
@@ -53,16 +91,8 @@ def fetch_data_dog_api():
 
 
 if __name__ == '__main__':
+        table_creation()
         etl_dog_api()
+        etl()
 
-    # def fetch_data():
-    #     url = 'https://api.fda.gov/animalandveterinary/event.json?limit=1000'
-    #     response = requests.get(url)
-    #     print(url)
-    #     yield response.json().get("results")
-    #
-    #     while response.links.get("next") is not None:
-    #         url = response.links.get('next').get("url")
-    #         response = requests.get(url)
-    #         yield response.json().get("results")
 
