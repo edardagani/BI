3568a0698c416b61548908cbdb3e1c73d91d05eb
Bill <billmewtwo1996@gmail.com>
2022-01-17 23:38:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from dash.dependencies import Input, Output
 from dash import dcc
 from dash import html
 from dash import dash_table
-
+from norm_old import *
 import pandas as pd
 import psycopg2
-from norm import *
+from norm_old import *
 
 
 
@@ -19,7 +19,7 @@ app = dash.Dash(external_stylesheets=external_stylesheet)
 breed = common_breeds()
 
 
-print(breed)
+# print(breed)
 
 df = pd.DataFrame(breed)
 
@@ -36,38 +36,62 @@ number_of_active_ingredients = df2[0].value_counts().values
 names_of_active_ingredients = df2[0].value_counts().index
 number_of_active_ingredients = number_of_active_ingredients[0:6]
 names_of_active_ingredients = names_of_active_ingredients[0:6]
-print(names_of_active_ingredients)
-print(number_of_active_ingredients)
+# print(names_of_active_ingredients)
+# print(number_of_active_ingredients)
+
+common_reactions = common_reactions()
+df3 = pd.DataFrame(common_reactions)
+common_reactions = df3[0].value_counts().values
+common_reactions_names = df3[0].value_counts().index
+common_reactions = common_reactions[0:6]
+common_reactions_names = common_reactions_names[0:6]
+
+duration = duration()
+df4 = pd.DataFrame(duration)
+duration = df4[0][0:50]
+print(duration)
+
+
+age = age()
+df5 = pd.DataFrame(age)
+age = df5[0][0:50]
 
 # test = df.head().to_dict('list')
 # print("ST2 Reqeust : {}".format(test))
 
 app.layout = html.Div(
-    children=[
-    html.H1('BI APP PLEZ WORK'),
-    html.Br(),
-    html.H3("My Visualizations"),
-    html.Div(
         children=[
-            dcc.Graph(
-                figure=dict(
-                    data=[dict(
-                        x=names_of_breeds.values.tolist(),
-                        y=number_of_breeds.tolist(),
-                        name='Most common Breed',
-                        type='bar'
-                    )],
-                    layout=dict(
-                        title='Most Common Breeds'
+            html.H1('BI APP PLEZ WORK'),
+            html.Br(),
+            html.H3("My Visualizations"),
+            html.Div(
+                children=[
+                    dcc.Graph(
+                        figure=dict(
+                            data=[
+                                dict(
+                                    x=names_of_breeds.values.tolist(),
+                                    y=number_of_breeds.tolist(),
+                                    name='Most common Breed',
+                                    type='bar'
+                                ),
+                                dict(
+                                    x=names_of_active_ingredients.values.tolist(),
+                                    y=number_of_active_ingredients.tolist(),
+                                    name='Most Active Ingredients',
+                                    type='bar'
+                                )
+                            ],
+                            layout=dict(
+                                title='Most Common Active Ingredients / Breeds'
+                            )
+                        ),
+                        id='breed'
                     )
-
-                ),
-                id='breed'
-            )
-        ]
-    ),
-        html.Div(
-            children=[
+                ]
+            ),
+            html.Div(
+                children=[
                 dcc.Graph(
                     figure=dict(
                         data=[dict(
@@ -84,7 +108,93 @@ app.layout = html.Div(
                     id='common-ingredient'
                 )
             ]
-        )
+        ),
+            html.Div(
+                children=[
+                    dcc.Graph(
+                        figure=dict(
+                            data=[
+                                dict(
+                                x="Number of Days",
+                                y=duration.tolist(),
+                                name='Duration',
+                                type='line'
+                            )],
+                            layout=dict(
+                                title='Duration'
+                            )
+
+                        ),
+                        id='duration-days'
+                    )
+                ]
+            ),
+            html.Div(
+                children=[
+                dcc.Graph(
+                    figure=dict(
+                        data=[
+                            dict(
+                                x=names_of_breeds.values.tolist(),
+                                y=common_reactions.tolist(),
+                                name='Most Common Reactions',
+                                marker=dict(
+                                    color='rgb(177, 35, 5)'
+                                ),
+                                type='bar'
+                            ),
+                            dict(
+                                x=common_reactions_names.values.tolist(),
+                                y=number_of_breeds.tolist(),
+                                name='Most Common Breeds',
+                                marker=dict(
+                                    color='rgb(26, 118, 255)'
+                                ),
+                                type='bar'
+                            ),
+                        ],
+                        layout=dict(
+                            title='God help us',
+                            showlegends=True
+                        )
+                    ),
+                    id='Common-reactions-breeds'
+                )
+            ]
+        ),
+            html.Div(
+                children=[
+                    dcc.Graph(
+                        figure=dict(
+                            data=[
+                                dict(
+                                    x="Number of Days",
+                                    y=age.tolist(),
+                                    name='Age in Years',
+                                    marker=dict(
+                                        color='rgb(177, 35, 5)'
+                                    ),
+                                    # type='bar'
+                                ),
+                                dict(
+                                    x="Number of Days",
+                                    y=duration.tolist(),
+                                    name='Duration In Days',
+                                    marker=dict(
+                                        color='rgb(26, 118, 255)'
+                                    ),
+                                    # type='bar'
+                                ),
+                            ],
+                            layout=dict(
+                                title='Age / Duration',
+                                showlegends=True
+                            )
+                        ),
+                        id='Age-Duration-id'
+                    )
+                ]
+            )
     ]
 )
 
